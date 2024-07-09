535030b5fff9db6298ec4a0786eda6c062cd06a8
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2024-07-08 22:11:30 -0700

    Changed Puzzle Collison Detection
    Also added a check to see if puzzle piece found it's drop area pair in order to lock it in place

--- Memoria/Assets/Scripts/PuzzlePiece.cs
+++ Memoria/Assets/Scripts/PuzzlePiece.cs
@@ -1,40 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzlePiece : MonoBehaviour
 {
     Vector3 offset;
     Collider2D puzzleCollider;
+    Image puzzleImage;
+    float collisionDist = 1.0f;
+    bool lockPiece = false;
     public GameObject dropArea = null;
     void Awake()
     {
         puzzleCollider = GetComponent<Collider2D>();
+        puzzleImage = GetComponent<Image>();
     }
 
     void OnMouseDown()
     {
+        if (lockPiece) return;
+
         transform.position = MouseWorldPosition();
     }
 
     void OnMouseDrag()
     {
+        if (lockPiece) return;
+
         transform.position = MouseWorldPosition() + offset;
+        puzzleImage.color = new Color(puzzleImage.color.r, puzzleImage.color.g, puzzleImage.color.b, 0.5f);
     }
 
     void OnMouseUp()
     {
+        float distance = Vector3.Distance(transform.position, dropArea.transform.position);
         puzzleCollider.enabled = false;
-        var rayOrigin = Camera.main.transform.position;
-        var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
-        RaycastHit2D hitInfo;
-        if (hitInfo = Physics2D.Raycast(rayOrigin, rayDirection))
+
+        if (distance < collisionDist)
         {
-            if (hitInfo.collider.gameObject == dropArea)
-            {
-                Debug.Log($"Found Pair: {name} / {hitInfo.collider.gameObject.name}");
-                transform.position = hitInfo.transform.position + new Vector3(0, 0, -0.01f);
-            }
+            Debug.Log($"Found Pair: {name} / {dropArea.name}");
+            transform.position = dropArea.transform.position + new Vector3(0, 0, -0.01f);
+            lockPiece = true;
         }
         puzzleCollider.enabled = true;
+        puzzleImage.color = new Color(puzzleImage.color.r, puzzleImage.color.g, puzzleImage.color.b, 1f);
     }
 
     Vector3 MouseWorldPosition()
