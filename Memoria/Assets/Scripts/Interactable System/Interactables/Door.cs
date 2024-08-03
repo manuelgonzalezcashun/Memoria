0988599e5dc18c01ea29f26aba9347b49d0fd7f8
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2024-08-02 23:21:26 -0700

    Update Door.cs

--- Memoria/Assets/Scripts/Interactable System/Interactables/Door.cs
+++ Memoria/Assets/Scripts/Interactable System/Interactables/Door.cs
@@ -6,7 +6,7 @@ public class Door : Interactable
     [SerializeField] private SceneReference roomToLoad;
     public override void Interact()
     {
-        if (roomToLoad != null)
+        if (roomToLoad == null)
         {
             Debug.LogWarning($"{name} is missing a scene reference. Please assign a scene reference!");
             return;
